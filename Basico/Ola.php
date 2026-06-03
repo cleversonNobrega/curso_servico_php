4a8cd0850837215bacfc6707fe310b40834d405f
Cleverson <felipecleverson1@gmail.com>
2026-06-03 14:38:40 -0300

    curso

--- Basico/Ola.php
+++ Basico/Ola.php
@@ -1,1 +1,12 @@
 <div class="Titulo">Olá PHP</div>
+
+
+<h2>Exemplo A</h2>
+<?php
+    echo 'Olá';
+    echo "\n";
+    echo 'Mundo!';
+?>
+
+<h2>Exemplo B</h2>
+<?=  "Outro forma de me 'Expressar!'" ?>
