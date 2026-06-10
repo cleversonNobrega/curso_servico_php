3ff35c38a2341eac2702e63fa926750436d6e3de
Cleverson <felipecleverson1@gmail.com>
2026-06-10 17:39:27 -0300

    curso

---
+++ Tipo_Basico/desafioPrecedencia.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '1. Qual o valor será impresso';
+
+echo("\n");
+
+echo 2 + 5 * 3 + (12/6) / (-8 + 2 ** 2);
+
+echo("\n");
+
+echo '2. Qual expressão imprime o valor 100?';
+
+echo("\n");
+
+echo 'a)', (1 + 2) * 20 - 15;
+echo("\n");
+echo 'b)', 4 * 4 ** 2;
+echo("\n");
+echo 'c)', 2 ** 3 ** 4 / 1e25;
+echo("\n");
+echo 'd)', 3 + (3 * 8 ) / 2 - 3 ;
