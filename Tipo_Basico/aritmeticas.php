f1a2ba7b56a01027c6b767a03b2c42987fe0c624
Cleverson <felipecleverson1@gmail.com>
2026-06-10 17:21:18 -0300

    curso

---
+++ Tipo_Basico/aritmeticas.php
@@ -0,0 +1,63 @@
+<?php
+
+echo 1 + 1 ;
+
+echo "\n";
+
+var_dump(1 + 1.0);
+
+echo 1 + 2.5;
+
+echo "\n";
+
+echo 10 - 2;
+
+echo "\n";
+
+echo 2 * 5;
+
+echo "\n";
+
+echo 7 / 4;
+
+echo "\n";
+
+echo intdiv(7,4);
+
+echo "\n";
+
+echo round(7 /4);
+
+echo "\n";
+
+echo 7 % 4;
+
+echo "\n";
+
+echo 7 % 2;
+
+echo "\n";
+
+echo 8 % 2;
+
+echo "\n";
+
+// echo intdiv(7,0);  erro! 
+
+echo "Procedência de operadores:";
+
+echo "\n";
+
+echo 2 + 3 * 4;
+
+echo "\n";
+
+echo (2 + 3) * 4;
+
+echo "\n";
+
+echo 2 + 3 * 4 ** 2;
+
+echo "\n";
+
+echo ((2 + 3) * 4) ** 2;
