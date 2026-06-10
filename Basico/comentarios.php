bd48f71bf604a7e9e2925a5c306c3971065b1a52
Cleverson <felipecleverson1@gmail.com>
2026-06-10 14:42:13 -0300

    curso

---
+++ Basico/comentarios.php
@@ -0,0 +1,18 @@
+<div class="titulo">Comentario</div>
+
+<?php 
+   // echo "Estou no PHP";
+   # echo "Estou no PHP";
+?>
+
+<p>Depois do comentario bloco!</p>
+
+<?php  
+/*
+    Linha 1 do comentário
+    Linha 2 do comentário
+    Linha 3 do comentário
+*/
+?>
+
+<p>Depois do segundo bloco!</p>
