24b5e9f3c973d78bbf285dce3a7c73c47dd147af
Cleverson <felipecleverson1@gmail.com>
2026-06-12 16:33:58 -0300

    curso

---
+++ Variaveis/atribuicoes.php
@@ -0,0 +1,50 @@
+<?php
+
+$title = 'atribuições';
+
+$numero = 10;
+
+$numero = $numero - 3;
+
+echo $numero;
+
+echo ("\n");
+
+$numero = $numero + 1.5;
+
+echo $numero;
+
+$numero--;
+
+echo ("\n");
+
+echo $numero;
+
+$numero++;
+
+echo ("\n");
+
+echo $numero;
+
+$numero += 5;
+
+echo ("\n");
+
+echo $numero;
+
+echo ("\n");
+
+
+$texto = 'esse é um texto';
+
+
+$texto = $texto . ' qualquer';
+
+$variavelInexistente = 'Valor existente';
+
+$valor = $variavelInexistente ?? 'valor default';
+
+echo $valor;
+
+
+echo $texto;
