e9ac357178d6861a052b2be8ebca16a1dc13c34f
Cleverson <felipecleverson1@gmail.com>
2026-06-12 15:19:28 -0300

    curso

---
+++ Variaveis/basico.php
@@ -0,0 +1,23 @@
+<?php
+
+$numeroA = 13;
+
+echo $numeroA;
+echo ("\n");
+var_dump($numeroA);
+
+$a = 3;
+$b = 16;
+
+$soma = $a + $b;
+
+echo isset($soma);
+
+unset($soma);
+var_dump($soma);
+
+$variavel = 10;
+
+$variavel = "Agora sou uma string ";
+
+echo $variavel;
